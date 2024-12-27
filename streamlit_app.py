a1bca52491a9a15fa363621947c6d1ac87a9fa46
Nancy398 <nancy@moohousing.com>
2024-12-26 16:57:29 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,7 @@ import pypandoc
 from num2words import num2words  # 用于将数字转换为英文大写
 from datetime import datetime
 from docx import Document
+import pandoc
 
 # 加载本地模板
 def load_template(file_path="Late Rent Notice Template.docx"):
